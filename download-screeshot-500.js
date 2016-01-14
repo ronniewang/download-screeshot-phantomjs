5416a20f66b1336a27917d10d6b39d9f318fb346
ronniewang <wangshengyu@caiex.com>
2016-01-14 16:23:53 +0800

    add date format

--- download-screeshot-500.js
+++ download-screeshot-500.js
@@ -4,26 +4,45 @@
 var fs = require('fs');
 var args = require('system').args;
 var page = require('webpage').create();
-
+var CATCH_PERIOD_MILLS = 60000;
 var tracker = (function () {
     var openRequests = [],
         counter = 0;
 
     return {
-        start: function (id) {
+        start : function (id) {
             openRequests[id] = true;
             counter++;
         },
-        end: function (id) {
+        end : function (id) {
             openRequests[id] = null;
             delete openRequests[id];
             counter--;
         },
-        count: function () {
+        count : function () {
             return counter;
         }
     };
 })();
+
+Date.prototype.format = function(fmt)
+{ //author: meizz   
+    var o = {
+        "M+" : this.getMonth()+1,                 //月份
+        "d+" : this.getDate(),                    //日
+        "h+" : this.getHours(),                   //小时
+        "m+" : this.getMinutes(),                 //分
+        "s+" : this.getSeconds(),                 //秒
+        "q+" : Math.floor((this.getMonth()+3)/3), //季度
+        "S"  : this.getMilliseconds()             //毫秒
+    };
+    if(/(y+)/.test(fmt))
+        fmt=fmt.replace(RegExp.$1, (this.getFullYear()+"").substr(4 - RegExp.$1.length));
+    for(var k in o)
+        if(new RegExp("("+ k +")").test(fmt))
+            fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substr((""+ o[k]).length)));
+    return fmt;
+}
 
 t = Date.now();
 if (args.length < 3) {
@@ -42,59 +61,61 @@ page.onResourceReceived = function (res) {
         console.log('[Response ' + res.id + '] ' + res.url);
     }
 };
+setInterval(function(){
+    page.open(args[1], function (status) {
+        if (status !== 'success') {
+            console.log('Unable to access network');
+        } else {
+            console.log('[page load status] ' + JSON.stringify(status));
 
-page.open(args[1], function (status) {
-    if (status !== 'success') {
-        console.log('Unable to access network');
-    } else {
-        console.log('[page load status] ' + JSON.stringify(status));
+            var intervalId,
+                timeoutId;
 
-        var intervalId, timeoutId;
+            intervalId = window.setInterval(function () {
+                if (tracker.count() === 0) {
+                    window.clearInterval(intervalId);
+                    window.clearTimeout(timeoutId);
 
-        intervalId = window.setInterval(function () {
-            if (tracker.count() === 0) {
+                    console.log('All clear!');
+                    phantom.exit(status !== 'success' ? 1 : 0);
+                }
+            }, 750);
+
+            window.setTimeout(function () {
                 window.clearInterval(intervalId);
                 window.clearTimeout(timeoutId);
 
-                console.log('All clear!');
-                phantom.exit(status !== 'success' ? 1 : 0);
-            }
-        }, 750);
-
-        window.setTimeout(function () {
-            window.clearInterval(intervalId);
-            window.clearTimeout(timeoutId);
-
-            var openReqCount = tracker.count();
-            if (openReqCount > 0) {
-                console.log('Still waiting on ' + openReqCount + ' open requests to end.');
-            } else {
-                console.log('All clear after 5 seconds....');
-            }
-            console.log('arg is ' + args[1])
-            page.render(getTimestamp(t) + '.jpg', {
-                format: 'jpg',
-                quality: '100'
-            })
-            var p = page.evaluate(function () {
-                return '<html>' + document.getElementsByTagName('html')[0].innerHTML + '</html>'
-            });
-            try {
-                fs.write(getTimestamp(t) + '.html', p, {
-                    mode: 'w',
-                    charset: args[2]
+                var openReqCount = tracker.count();
+                if (openReqCount > 0) {
+                    console.log('Still waiting on ' + openReqCount + ' open requests to end.');
+                } else {
+                    console.log('All clear after 5 seconds....');
+                }
+                console.log('arg is ' + args[1])
+                page.render(getTimestamp(t) + '.jpg', {
+                    format : 'jpg',
+                    quality : '100'
+                })
+                var p = page.evaluate(function () {
+                    return '<html>' + document.getElementsByTagName('html')[0].innerHTML + '</html>'
                 });
-            } catch (e) {
-                console.log(e);
-            }
-            t = Date.now() - t;
-            console.log('Loading time ' + t + ' millsec');
-            phantom.exit(status !== 'success' ? 1 : 0);
-        }, 5000);
-    }
-});
-
+                try {
+                    fs.write(getTimestamp(t) + '.html', p, {
+                        mode : 'w',
+                        charset : args[2]
+                    });
+                } catch (e) {
+                    console.log(e);
+                }
+                t = Date.now() - t;
+                console.log('Loading time ' + t + ' millsec');
+                //phantom.exit(status !== 'success' ? 1 : 0);
+            }, CATCH_PERIOD_MILLS);
+        }
+    });
+}, 10000);
 function getTimestamp(t) {
-    t = new Date(t);
-    return t.getFullYear() + "" + (t.getMonth() + 1) + "" + t.getDate() + "_" + t.getHours() + "" + t.getMinutes() + "" + t.getSeconds();
+    d = new Date(t);
+    return d.format('yyyyMMdd_hhmmss');
+    //return t.getFullYear() + "" + (t.getMonth() + 1) + "" + t.getDate() + "_" + t.getHours() + "" + t.getMinutes() + "" + t.getSeconds();
 }
